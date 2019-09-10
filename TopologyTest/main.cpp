b23984e4fe49b0f42b4d6fb51d763fb035c68950
Jorge <jpordonezandrade@gmail.com>
2019-09-10 11:23:14 -0300

    mesh refinement prototype

--- TopologyTest/main.cpp
+++ TopologyTest/main.cpp
@@ -49,7 +49,8 @@
 #include "TPZExtendGridDimension.h"
 #include "pzshapetriang.h"
 #include "pzshapequad.h"
-
+#include "pzrefquad.h"
+TPZGeoMesh *GenerateMeshRef(double l, double h, int side, int nivel);
 void TestTriangleTransforms();
 using namespace std;
 //typedef REAL RND_MAX;
@@ -57,8 +58,9 @@ REAL RND_MAX = 4294967295.0;
 
 
 int main(){
+    GenerateMeshRef(2,2, 4, 4);
     
-  TestTriangleTransforms();
+//  TestTriangleTransforms();
     
     
 //    pztopology::TPZQuadrilateral trian;
@@ -133,3 +135,108 @@ void TestTriangleTransforms(){
     
     
 }
+
+TPZGeoMesh *GenerateMeshRef(double l, double h, int side, int nivel){
+    TPZVec<REAL> p0(3,0.0);
+    TPZVec<REAL> p1(3,0.0);
+    p1[0]= l;
+    p1[1]= h;
+    TPZVec<int> nx(2,1);
+    
+    TPZGenGrid gen(nx, p0, p1);
+    TPZGeoMesh *gmsh = new TPZGeoMesh;
+    gen.Read(gmsh);
+    gmsh->BuildConnectivity();
+    double val = h;
+    int count = 0;
+    if (side == 4) {
+        for (count=0; count<nivel; count++) {
+            val= val/2.0;
+            int nels = gmsh->NElements();
+            for (int j=0; j<nels; j++) {
+                TPZGeoEl *gel = gmsh->Element(j);
+                if (gel->HasSubElement()) {
+                    continue;
+                }
+                TPZFMatrix<REAL> cooridnates;
+                gel->NodesCoordinates(cooridnates);
+                cooridnates.Print(std::cout);
+                if (cooridnates(1,0)< val) {
+                    TPZVec<TPZGeoEl *> filios;
+                    gel->Divide(filios);
+                }
+                
+//                TPZVec<TPZGeoEl *> filhos;
+//                gel->Divide(filhos);
+            }
+        }
+   
+    }
+ 
+//    for (int i=0; i<nivel; i++) {
+//        int nels = gmsh->NElements();
+//        for (int j=0; j<nels; j++) {
+//            TPZGeoEl *gel = gmsh->Element(j);
+//            if (gel->HasSubElement()) {
+//                continue;
+//            }
+//            TPZVec<TPZGeoEl *> filhos;
+//            gel->Divide(filhos);
+//        }
+//
+//    }
+    
+    std::cout<<gmsh->NElements()<<endl;
+    std::ofstream file("2dGeomesh.vtk");
+    TPZVTKGeoMesh::PrintGMeshVTK(gmsh, file);
+    return gmsh;
+    
+}
+
+void Divide(TPZGeoEl *gel, int side, double val){
+    TPZFMatrix<REAL> cooridnates;
+    TPZGeoMesh *gmesh = gel->Mesh();
+    gel->NodesCoordinates(cooridnates);
+    TPZVec<int64_t> nodeindices;
+    gel->GetNodeIndices(nodeindices);
+    int64_t index1;
+    pzrefine::TPZRefQuad::NewMidSideNode(gel, 5, index1);
+    int64_t index2;
+    pzrefine::TPZRefQuad::NewMidSideNode(gel, 7, index2);
+    
+    
+    
+//    gel->MidSideNodeIndex(5, )
+//    TPZGeoEl *subel = gel->Mesh()->CreateGeoElement(EQuadrilateral,cornerindexes,matid,index,0);
+//    tpzrefquad
+//    gel->SetSubElement(i , subel);
+}
+
+TPZGeoNode *Geonode(TPZGeoMesh *gmsh, int side){
+    
+//    MidSideNodeIndex(gel,side,index);
+//    if(index < 0) {
+//        TPZGeoElSide gelside = gel->Neighbour(side);
+//        if(gelside.Element()) {
+//            while(gelside.Element() != gel) {
+//                gelside.Element()->MidSideNodeIndex(gelside.Side(),index);
+//                if(index!=-1) return;
+//                gelside = gelside.Neighbour();
+//            }
+//        }
+//        TPZVec<REAL> par(3,0.);
+//        TPZVec<REAL> coord(3,0.);
+//        if(side < TPZShapeQuad::NCornerNodes) {
+//            index = gel->NodeIndex(side);
+//            return;
+//        }
+//        //aqui side = 8 a 26
+//        side-=TPZShapeQuad::NCornerNodes;//0,1,..,18
+//        par[0] = MidCoord[side][0];
+//        par[1] = MidCoord[side][1];
+//        gel->X(par,coord);
+//        index = gel->Mesh()->NodeVec().AllocateNewElement();
+//        gel->Mesh()->NodeVec()[index].Initialize(coord,*gel->Mesh());
+    
+    return 0;
+};
